39fc808a7302efa9477714e440a3076f4c5f1261
viktorbeznosov <viktorbeznosov@mail.ru>
2023-05-28 15:12:46 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,30 +1,43 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
 
-    private int size;
+    private int sizeIn;
+    private int sizeOut;
 
     public T poll() {
-        if (size == 0) {
+        if (sizeIn + sizeOut == 0) {
             throw new NoSuchElementException("Queue is empty");
         }
-        for (int i = 0; i < size; i++) {
-            out.push(in.pop());
+        if (sizeOut == 0) {
+            for (; sizeIn > 0; sizeIn--) {
+                out.push(in.pop());
+                sizeOut++;
+            }
         }
+
         T elem = out.pop();
-        size--;
-        for (int i = 0; i < size; i++) {
-            in.push(out.pop());
-        }
+        sizeOut--;
         return elem;
     }
 
     public void push(T value) {
         in.push(value);
-        size++;
+        sizeIn++;
+    }
+
+    public static void main(String[] args) {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.poll());
+        queue.push(3);
+
+        System.out.println(queue.poll());
     }
 }
