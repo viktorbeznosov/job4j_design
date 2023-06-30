0601ba96814adbcd912d17a663a01ab114401039
viktorbeznosov <viktorbeznosov@mail.ru>
2023-06-30 23:09:23 +0300

    5.1. BufferedReader [#252489  [#252489]]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        List<String> result = new LinkedList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            in.lines()
+                .filter(e -> {
+                    String[] elements = e.split(" ");
+                    return elements[elements.length - 2].equals("404");
+                })
+                .forEach(result::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("data/log.txt");
+        log.stream().forEach(System.out::println);
+    }
+}
--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,23 +1,14 @@
 package ru.job4j.io;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ReadFile {
 
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("data/input.txt")) {
-            StringBuilder text = new StringBuilder();
-            int read;
-            while ((read = in.read()) != -1) {
-                text.append((char) read);
-            }
-            System.out.println(text);
-            String[] lines = text.toString().split(System.lineSeparator());
-            for (String line : lines) {
-                System.out.println(line);
-            }
-        } catch (IOException e) {
+        try (BufferedReader in = new BufferedReader(new FileReader("data/input.txt"))) {
+            in.lines().forEach(System.out::println);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
