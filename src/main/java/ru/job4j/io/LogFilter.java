46f800f2f7107ad9ca430b4e01221a5f0f68d199
viktorbeznosov <viktorbeznosov@mail.ru>
2023-07-01 22:20:23 +0300

    5.2. BufferedOutputStream [#252490  [#252490]]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,13 +1,12 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LogFilter {
-    public List<String> filter(String file) {
+    public static List<String> filter(String file) {
         List<String> result = new LinkedList<>();
         try (BufferedReader in = new BufferedReader(new FileReader(file))) {
             in.lines()
@@ -22,9 +21,19 @@ public class LogFilter {
         return result;
     }
 
+    public static void save(List<String> log, String file) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream(file)
+                ))) {
+            log.forEach(out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        LogFilter logFilter = new LogFilter();
-        List<String> log = logFilter.filter("data/log.txt");
-        log.stream().forEach(System.out::println);
+        List<String> log = filter("data/log.txt");
+        save(log, "data/404.txt");
     }
 }
--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
     public static int[][] multiple(int size) {
@@ -16,16 +18,14 @@ public class ResultFile {
     }
 
     public static void main(String[] args) {
-        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
-            int[][] matrix = multiple(10);
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix[i].length; j++) {
-                    String result = matrix[i][j] + " ";
-                    out.write(result.getBytes());
-                }
-                out.write(System.lineSeparator().getBytes());
-            }
-        } catch (IOException e) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
+            out.printf("%s%n", "Some string");
+            out.printf("%d%n", 10);
+            out.printf("%f%n", 1.5f);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
