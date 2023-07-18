97bcd6fbe392c3bf2b45ee41fe87decc72427dda
viktorbeznosov <viktorbeznosov@mail.ru>
2023-07-18 18:46:38 +0300

    12. Валидация параметров запуска [#246865  [#246865 #430502]]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -24,11 +24,30 @@ public class Search {
         if (args.length < 2) {
             throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
         }
-        if (!Files.exists(Paths.get(args[0]))) {
+        if (!Files.exists(Paths.get(args[0])) || !Files.isDirectory(Paths.get(args[0]))) {
             throw new IllegalArgumentException("Folder is not exists");
         }
-        if (!args[1].startsWith(".")) {
+        if (!checkExtension(args[1])) {
             throw new IllegalArgumentException("Invalid file extension");
         }
+    }
+
+    private static boolean checkExtension(String str) {
+        boolean result = true;
+        if (str.length() < 2 || !str.startsWith(".")) {
+            result = false;
+        }
+        if (result) {
+            str = str.replaceFirst(".", "");
+            for (char c: str.toCharArray()) {
+                System.out.println((int) c);
+                if (((int) c < 141 || (int) c > 172) && ((int) c < 101 || (int) c > 132)) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
