7a13b11c9c244563565c122984cffc707dd79c6c
viktorbeznosov <viktorbeznosov@mail.ru>
2023-07-25 14:57:05 +0300

    16. Кодировка [#862  [#862 #432009]]

--- src/main/java/ru/job4j/encode/ConsoleChat.java
+++ src/main/java/ru/job4j/encode/ConsoleChat.java
@@ -10,10 +10,12 @@ public class ConsoleChat {
     private static final String CONTINUE = "продолжить";
     private final String path;
     private final String botAnswers;
+    private List<String> phrases;
 
     public ConsoleChat(String path, String botAnswers) {
         this.path = path;
         this.botAnswers = botAnswers;
+        this.phrases = readPhrases();
     }
 
     public void run() {
@@ -21,23 +23,18 @@ public class ConsoleChat {
         Scanner in = new Scanner(System.in);
         String line = "";
         List<String> log = new ArrayList<>();
-        while (!line.equals(OUT)) {
+        Random random = new Random();
+        while (!OUT.equals(line)) {
             line = in.nextLine();
             log.add(line);
             if (!stop && STOP.equals(line) || stop && CONTINUE.equals(line)) {
                 stop = !stop;
             }
             if (!stop) {
-                System.out.println(getRandomPhrase());
+                System.out.println(phrases.get(random.nextInt(phrases.size())));
             }
         }
         saveLog(log);
-    }
-
-    private String getRandomPhrase() {
-        List<String> phrases = readPhrases();
-        Random random = new Random();
-        return phrases.get(random.nextInt(phrases.size()));
     }
 
     private List<String> readPhrases() {
@@ -51,12 +48,10 @@ public class ConsoleChat {
     }
 
     private void saveLog(List<String> log) {
-        StringBuilder builder = new StringBuilder();
-        for (String str: log) {
-            builder.append(str).append(System.lineSeparator());
-        }
         try (PrintWriter pw = new PrintWriter(new FileWriter(path, true))) {
-            pw.println(builder.toString());
+            for (String str: log) {
+                pw.println(str);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
