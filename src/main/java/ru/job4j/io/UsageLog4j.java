269037f1b6631c01ec8acf2c668a8d18a6576432
viktorbeznosov <viktorbeznosov@mail.ru>
2023-08-01 21:17:57 +0300

    4. Slf4j - вывод exception. [#268853  [#268853]]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,17 +7,10 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Viktor Beznosov";
-        byte age = 44;
-        short daysOnJob4j = 217;
-        int maxInt = 2147483647;
-        char sex = 'М';
-        boolean adult = true;
-        long fromEarthToSun = 149_600_000_000L;
-        float height = 177.5f;
-        double someDouble = 0.34535d;
-
-        LOG.info("User info name : {}, age : {}, days in Job4j : {}, sex : {}, adult : {}, height: {}", name, age, daysOnJob4j, sex, adult, height);
-        LOG.debug("Max int in Java - {}, some double number - {}", maxInt, someDouble);
+        try {
+            throw new Exception("Not supported code");
+        } catch (Exception e) {
+            LOG.error("Exception in log example", e);
+        }
     }
 }
