074135666b897bb0ec20a53af2d7dde189b88e26
viktorbeznosov <viktorbeznosov@mail.ru>
2023-08-05 18:22:14 +0300

    4. JAXB. Преобразование XML в POJO. [#315063  [#315063]]

--- src/main/java/ru/job4j/serialization/xml/Main.java
+++ src/main/java/ru/job4j/serialization/xml/Main.java
@@ -1,7 +1,27 @@
 package ru.job4j.serialization.xml;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
+import java.io.StringWriter;
+
 public class Main {
-    public static void main(String[] args) {
-        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+    public static void main(String[] args) throws Exception {
+        Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        String xml = "";
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            xml = writer.getBuffer().toString();
+            System.out.println(xml);
+        }
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        try (StringReader reader = new StringReader(xml)) {
+            Person result = (Person) unmarshaller.unmarshal(reader);
+            System.out.println(result);
+        }
     }
 }
