6d40fb6e0ea5103e7285a1d73d7968051a22c376
viktorbeznosov <viktorbeznosov@mail.ru>
2023-08-06 13:17:54 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064  [#315064]]

--- src/main/java/ru/job4j/serialization/json/Main.java
+++ src/main/java/ru/job4j/serialization/json/Main.java
@@ -13,49 +13,49 @@ public class Main {
         final Person person = new Person(false, 30, new Contact("11-111"),
                 new String[] {"Worker", "Married"});
 
-//        final Gson gson = new GsonBuilder().create();
-//        System.out.println(gson.toJson(person));
-//
-//        final String personJson =
-//                "{"
-//                        + "\"sex\":false,"
-//                        + "\"age\":35,"
-//                        + "\"contact\":"
-//                        + "{"
-//                        + "\"phone\":\"+7(924)111-111-11-11\""
-//                        + "},"
-//                        + "\"statuses\":"
-//                        + "[\"Student\",\"Free\"]"
-//                        + "}";
-//
-//        final Person personMod = gson.fromJson(personJson, Person.class);
-//        System.out.println(personMod);
-//
-//        final Product notebook = new Product("Acer Aspire 1 A115-22-R2DZ", 28990, true, new String[] {"Notebooks", "Computers"}, person);
-//        System.out.println(gson.toJson(notebook));
-//
-//        final String noteBookJson =
-//             "{"
-//                + "\"name\": \"Acer Aspire 1 A115-22-R2DZ\","
-//                + "\"price\": 28990,"
-//                + "\"available\": true,"
-//                + "\"categories\": ["
-//                +    "\"Notebooks\","
-//                +    "\"Computers\""
-//                + "],"
-//                + "\"salesManager\": {"
-//                +    "\"sex\": false,"
-//                +        "\"age\": 30,"
-//                +        "\"contact\": {"
-//                +        "\"phone\": \"11-111\""
-//                +    "},"
-//                +    "\"statuses\": ["
-//                +            "\"Worker\","
-//                +            "\"Married\""
-//                +    "]"
-//                + "}"
-//            + "}";
-//        System.out.println(gson.fromJson(noteBookJson, Product.class));
+        final Gson gson = new GsonBuilder().create();
+        System.out.println(gson.toJson(person));
+
+        final String personJson =
+                "{"
+                        + "\"sex\":false,"
+                        + "\"age\":35,"
+                        + "\"contact\":"
+                        + "{"
+                        + "\"phone\":\"+7(924)111-111-11-11\""
+                        + "},"
+                        + "\"statuses\":"
+                        + "[\"Student\",\"Free\"]"
+                        + "}";
+
+        final Person personMod = gson.fromJson(personJson, Person.class);
+        System.out.println(personMod);
+
+        final Product notebook = new Product("Acer Aspire 1 A115-22-R2DZ", 28990, true, new String[] {"Notebooks", "Computers"}, person);
+        System.out.println(gson.toJson(notebook));
+
+        final String noteBookJson =
+             "{"
+                + "\"name\": \"Acer Aspire 1 A115-22-R2DZ\","
+                + "\"price\": 28990,"
+                + "\"available\": true,"
+                + "\"categories\": ["
+                +    "\"Notebooks\","
+                +    "\"Computers\""
+                + "],"
+                + "\"salesManager\": {"
+                +    "\"sex\": false,"
+                +        "\"age\": 30,"
+                +        "\"contact\": {"
+                +        "\"phone\": \"11-111\""
+                +    "},"
+                +    "\"statuses\": ["
+                +            "\"Worker\","
+                +            "\"Married\""
+                +    "]"
+                + "}"
+            + "}";
+        System.out.println(gson.fromJson(noteBookJson, Product.class));
 
         /* JSONObject из json-строки строки */
         JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
