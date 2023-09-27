c659dbd002abc447f6015d372fdf966b3f41c5ff
viktorbeznosov <viktorbeznosov@mail.ru>
2023-09-27 22:21:38 +0300

    1. Связи между таблицами [#373615]

---
+++ db/relations.sql
@@ -0,0 +1,27 @@
+create table if not exists humans (
+    id serial primary key,
+    name varchar
+);
+
+create table if not exists nations (
+    id serial primary key,
+    title varchar
+);
+
+alter table humans add column nation_id int references nations(id);
+
+create table if not exists countries (
+    id serial primary key,
+    name varchar
+);
+
+create table if not exists human_country (
+    human_id int references humans(id),
+    country_id int references countries(id)
+);
+
+create table if not exists capitals (
+    id serial primary key,
+    name varchar,
+    country_id int references countries(id)
+);
