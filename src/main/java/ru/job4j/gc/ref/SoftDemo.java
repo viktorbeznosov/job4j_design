5435624896b719597b8b7fd64574afba2383bdc7
viktorbeznosov <viktorbeznosov@mail.ru>
2024-01-08 17:40:03 +0300

    1. Реализация кеша на SoftReference [#1592]

--- src/main/java/ru/job4j/gc/ref/SoftDemo.java
+++ src/main/java/ru/job4j/gc/ref/SoftDemo.java
@@ -7,8 +7,7 @@ import java.util.List;
 public class SoftDemo {
 
     public static void main(String[] args) {
-//        example1();
-        example2();
+        example1();
     }
 
     private static void example1() {
--- src/main/java/ru/job4j/gc/ref/WeakDemo.java
+++ src/main/java/ru/job4j/gc/ref/WeakDemo.java
@@ -10,9 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class WeakDemo {
 
     public static void main(String[] args) throws InterruptedException {
-//        example1();
-//        example2();
-        example3();
+        example1();
     }
 
     private static void example1() throws InterruptedException {
