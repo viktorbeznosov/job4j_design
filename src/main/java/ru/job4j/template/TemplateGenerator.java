43deccb445c2d4de29549cd9278b570437aaa7c0
viktorbeznosov <viktorbeznosov@mail.ru>
2024-03-08 14:29:01 +0300

    4. Шаблонизатор. [#855]

---
+++ src/main/java/ru/job4j/template/TemplateGenerator.java
@@ -0,0 +1,11 @@
+package ru.job4j.template;
+
+import java.util.Map;
+
+public class TemplateGenerator implements Generator {
+
+    @Override
+    public String produce(String template, Map<String, String> args) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/job4j/template/TemplateGeneratorTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.job4j.map.SimpleMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TemplateGeneratorTest {
+
+    @Test
+    public void whenCorrectTemplateGenerate() {
+        TemplateGenerator generator = new TemplateGenerator();
+        String inputString = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        assertThat(generator.produce(inputString, args)).isEqualTo("I am a Petr Arsentev, Who are you?");
+    }
+
+    @Test
+    public void whenNotEnoughArgumentsThenGetException() {
+        TemplateGenerator generator = new TemplateGenerator();
+        String inputString = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        assertThatThrownBy(() -> generator.produce(inputString, args)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void whenIncorrectArgumentsThenGetException() {
+        TemplateGenerator generator = new TemplateGenerator();
+        String inputString = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("foo", "bar");
+        args.put("incorrect", "argument");
+        assertThatThrownBy(() -> generator.produce(inputString, args)).isInstanceOf(IllegalArgumentException.class);
+    }
+}
