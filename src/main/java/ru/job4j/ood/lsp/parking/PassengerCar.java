d8762d6f1ccdde9727f3b534c283e5a44d799ba8
viktorbeznosov <viktorbeznosov@mail.ru>
2024-05-19 16:07:09 +0300

    2. Парковка машин [#853 #496620]

--- src/main/java/ru/job4j/ood/lsp/parking/PassengerCar.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.ood.lsp.parking;
-
-public class PassengerCar extends AbstractCar {
-    public PassengerCar() {
-        this.type = CarType.PASSENGER_CAR;
-        this.size = 1;
-    }
-}
--- src/main/java/ru/job4j/ood/lsp/parking/PlaceDistributor.java
+++ src/main/java/ru/job4j/ood/lsp/parking/PlaceDistributor.java
@@ -4,10 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public interface PlaceDistributor {
-    final HashMap<CarType, PlaceDistributor> DISTRIBUTOR_MAP = new HashMap<CarType, PlaceDistributor>() {{
-       put(CarType.PASSENGER_CAR, new PassengerCarPlaceDistributor());
-       put(CarType.TRUCK, new TruckPlaceDistributor());
-    }};
+    final HashMap<CarType, PlaceDistributor> DISTRIBUTOR_MAP = new HashMap<CarType, PlaceDistributor>();
 
     public Map<CarType, Integer> distribute(Car car, Map<CarType, Integer> map) throws Exception;
 
